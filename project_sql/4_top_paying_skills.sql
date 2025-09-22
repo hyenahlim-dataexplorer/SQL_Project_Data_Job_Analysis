2d55eb645e88060e3577beb737a89158b8f6cb98
Hyenah (Hannah) Lim <hyenahlim@gmail.com>
2025-09-22 17:20:28 -0500

    Update 4_top_paying_skills.sql

--- project_sql/4_top_paying_skills.sql
+++ project_sql/4_top_paying_skills.sql
@@ -1,7 +1,7 @@
 /* Question: What are the top skills based on salary?
  - Look at the average salary associated with each skill for Data Analyst positions.
  - Focuses on roles with specified salaries, regardless of location.
- - Why? It reveals how different skilsl impact levels for Data Analysts and helps identify
+ - Why? It reveals how different skills impact levels for Data Analysts and helps identify
    most financially rewarding skills to acquire or improve.
 */
 
